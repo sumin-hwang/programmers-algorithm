24bf1639143d7cb7d7ac1323d2a290e1e854035d
sumin-hwang <suumiin.h@gmail.com>
2023-04-15 19:59:37 +0900

    에라토스테네스의 체 (수정)

--- getPrimeNumber.js
+++ getPrimeNumber.js
@@ -1,13 +1,14 @@
-const getPrimeNumber = (num) => {
-    const check = Array(num).fill(true);
 
-    for(let i = 2; i <= Math.sqrt(num); i++){
+function getPrime(num){
+    const check = Array(num+1).fill(true).fill(false, 0, 2);
+    
+    for(let i =2; i <= Math.sqrt(num); i++){
         if(check[i]){
-            for(let j = i + i; j <=num; j += i){
+            for(let j = i + i; j <= num; j +=i){
                 check[j] = false;
             }
         }
     }
-
-    return check;
+    
+    return check.filter(c=> c===true).length;
 }
