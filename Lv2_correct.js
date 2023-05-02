21ce91d93ee6e0091dea2712d8328444d577ae68
sumin-hwang <suumiin.h@gmail.com>
2023-05-03 00:17:31 +0900

    Lv2.올바른 괄호

---
+++ Lv2_correct.js
@@ -0,0 +1,13 @@
+function solution(s){
+    var answer = true;
+    var cnt = 0;
+    
+    for(let i =0; i < s.length; i++){
+        s[i] === '(' ? cnt++ : cnt--;
+        if(cnt < 0){
+            return false;
+        }
+    }
+    
+    return cnt === 0 ? true : false;
+}
