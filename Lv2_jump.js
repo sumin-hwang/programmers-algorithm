5abcd0fd2a92f79c59bab117b6ce494702cb1ebe
sumin-hwang <suumiin.h@gmail.com>
2023-06-19 23:15:28 +0900

    Lv2 점프와 순간이동

---
+++ Lv2_jump.js
@@ -0,0 +1,17 @@
+function solution(n)
+{
+    var ans = 1;
+
+    while(n !== 1){
+        if(n%2 === 0){
+            n = n/2;
+        }else{
+            n = (n-1)/2;
+            ans++;
+        }
+    }
+    
+    return ans;
+}
+
+
