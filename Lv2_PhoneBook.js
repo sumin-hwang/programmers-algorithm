233c011d312f3905a9d558895cdfe3ce30eac25a
sumin-hwang <suumiin.h@gmail.com>
2023-08-08 23:20:36 +0900

    Lv2. 전화번호부 목록

---
+++ Lv2_PhoneBook.js
@@ -0,0 +1,14 @@
+function solution(phone_book) {
+    var answer = true;
+    phone_book.sort();
+    
+    for(let i =1; i < phone_book.length; i++){
+        if(phone_book[i].startsWith(phone_book[i-1])){
+            answer = false;
+            break;
+        }
+    }
+    
+
+    return answer;
+}
