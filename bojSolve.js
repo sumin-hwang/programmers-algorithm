fbb44302f448b474a65472e6dc6980992d039b35
sumin-hwang <suumiin.h@gmail.com>
2024-10-15 22:01:13 +0900

    boj1978 prime number

--- bojSolve.js
+++ bojSolve.js
@@ -17,23 +17,23 @@ let inputSpaceNumber = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\al
 let input = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().trim().split('\n');
 let input1 = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().split('\n').map(c => c.split(" ").map(c => Number(c)));
 
-const cnt = input1[0];
+// let cnt = inputSpaceNumber[0];
+const isPrime = (n) => {
+    if( n===1){
+        return false;
+    }
 
-let arr = new Array(100).fill().map(()=> Array(100).fill(0));
-
-for(let i = 0; i < cnt; i++){
-    const [x, y] = input1[i+1];
-    for(let j = y; j < y + 10; j++){
-        for(let k = x; k < x + 10; k++){
-            arr[k][j] = 1;
+    for(let i =2; i <= Math.sqrt(n);i++){
+        if(n%i===0){
+            return false;
         }
     }
+
+    return true;
 }
 
-// let answer = arr[3].filter(c => c === 1).length;
-let answer = 0;
-for(let i =0 ; i < 100; i++){
-    answer += arr[i].filter(c => c ===1).length;
-}
+// const input123 = require('fs').readFileSync('/devs/stdin').toString().trim();
 
-console.log(answer);
+const[c1, nums] = input.split("\n");
+
+console.log(nums.split(" ").filter(x => isPrime(x)).length);
