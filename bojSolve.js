2cd939f1d78c340fd2c0bfdddb9f1fa20b3217b4
sumin-hwang <suumiin.h@gmail.com>
2025-04-07 23:54:42 +0900

    백준 2174 카드 (연결리스트 구현)

--- bojSolve.js
+++ bojSolve.js
@@ -20,34 +20,63 @@ let inputSpaceNumber = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\al
 let input = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().trim().split('\n');
 let input1 = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().split('\n').map(c => c.split(" ").map(c => Number(c)));
 
-let N = input[0];
-let arr = input[1].split(" ").map(c => Number(c));
+const num = Number(input[0]);
 
-let snack = [];
-let wait = [];
-
-let cnt = 1;
-let success = true;
-
-
-while(cnt <= N){
-    if(wait[wait.length -1 ] === cnt){
-        wait.pop();
-        cnt++;
-    }else{
-        if(arr.length === 0){
-            success = false;
-            break;
-        }else{
-            let now = arr.shift();
-
-            if(now === cnt){
-                cnt++;
-            }else{
-                wait.push(now);
-            }
-        }
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+        this.prev = null;
     }
 }
 
-success ? console.log('Nice') : console.log('Sad');
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    add(value){
+        const node = new Node(value);
+
+        if(this.head){
+            this.tail.next = node;
+            node.prev = this.tail;
+        }else{
+            this.head = node;
+        }
+
+        this.tail = node;
+        this.length++;
+        return node;
+    }
+
+    remove(){
+        this.head = this.head.next;
+        this.head.prev = null;
+        this.length--;
+    }
+
+    getHead(){
+        return this.head.value;
+    }
+}
+
+function solution(num) {
+    const cards = new LinkedList();
+    for(let i = 1; i <= num; i++){
+        cards.add(i);
+    }
+
+    while(cards.length > 1){
+        cards.remove();
+        cards.add(cards.getHead());
+        cards.remove();
+    }
+
+    return cards.getHead();
+}
+
+const card = solution(num);
+console.log(card);
