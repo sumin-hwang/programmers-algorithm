d5e700cc82cc02df5300e5700b9c5f6f6cc4b0a7
sumin-hwang <suumiin.h@gmail.com>
2025-04-09 23:51:10 +0900

    백준 28279 덱

--- boj_28279.js
+++ boj_28279.js
@@ -1,25 +1,3 @@
-// let fs = require('fs');
-// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-
-
-// const input = require("fs").readFileSync("/dev/stdin", "utf-8").trim().split("\n");
-
-
-
-//let input = require('fs').readFileSync('/dev/stdin').toString().trim()
-
-// const fs = require('fs');
-// const input = fs.readFileSync('/dev/stdin').toString().split('\n');
-
-// const a = require("fs").readFileSync("/dev/stdin")
-
-//배열을 받아서 숫자로 저장하기
-let inputNumberNL =require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().split('\n').map(c => Number(c));
-let inputStringNL = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().trim().split('\n');
-let inputSpaceNumber = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().trim().split(' ').map(c => Number(c));
-let input = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().trim().split('\n');
-let input1 = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().split('\n').map(c => c.split(" ").map(c => Number(c)));
-
 class Node {
     constructor(value) {
         this.value = value;
