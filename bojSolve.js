1c0c3e6ebdb87b2df4fde5e7639018dcc614cf66
sumin-hwang <suumiin.h@gmail.com>
2025-04-22 21:50:47 +0900

    백준 27433 팩토리얼2

--- bojSolve.js
+++ bojSolve.js
@@ -1,9 +1,6 @@
 // let fs = require('fs');
 // let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-const npmlog = require('npmlog');
-
-
 // const input = require("fs").readFileSync("/dev/stdin", "utf-8").trim().split("\n");
 
 // const input = require("fs")
@@ -27,28 +24,15 @@ let inputSpaceNumber = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\al
 let input = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().trim().split('\n');
 let input1 = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().split('\n').map(c => c.split(" ").map(c => Number(c)));
 
-const a = input.shift();
+const num = Number(input.shift());
 
-let map = new Map();
+function factorial(n) {
 
-input = input.map(c => c.trim());
-
-for(let e of input){
-
-    if(e.length >= 4){
-       if(map.has(e)){
-        map.set(e, map.get(e) + 1);
-       }else{
-        map.set(e, 1);
-       }
+    if(n===1 || n === 0){
+        return 1;
+    }else{
+        return n * factorial(n-1);
     }
 }
 
-let sortApp = [...map].sort((a,b) => b[1] - a[1]);
-
-let ans = [];
-for(e of sortApp){
-    ans.push(e[0]);
-}
-
-ans.map(c=> console.log(c));
+console.log(factorial(num));
---
+++ boj_27433.js
@@ -0,0 +1,12 @@
+const num = Number(input.shift());
+
+function factorial(n) {
+
+    if(n===1 || n === 0){
+        return 1;
+    }else{
+        return n * factorial(n-1);
+    }
+}
+
+console.log(factorial(num));
