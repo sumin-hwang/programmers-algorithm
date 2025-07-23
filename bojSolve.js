13663191c221b9605023f596ad52e32eba93bfb0
sumin-hwang <suumiin.h@gmail.com>
2025-07-23 23:36:30 +0900

    백준 2178 미로탐색 (bfs)

--- bojSolve.js
+++ bojSolve.js
@@ -25,51 +25,36 @@ let inputSpaceNumber = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\al
 let input = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().trim().split('\n');
 let input1 = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().split('\n').map(c => c.split(" ").map(c => Number(c)));
 
-
-const T = Number(input.shift());
-let count = 0;
-let answer = [];
+const [N, M] = input.shift().split(" ").map(Number);
+const graph = new Array(N);// visited배열 생성
+const visited = Array.from(Array(N), () => Array(M).fill(false)); // visited배열 생성
 
 const dx = [-1, 1, 0, 0];
-const dy = [0, 0, 1, -1];
+const dy = [0, 0, -1, 1];
 
-
-for(let i =0; i < T; i++){
-    const [M, N, K] = input.shift().split(" ").map(Number);
-    const graph = Array.from(Array(N), () => Array(M).fill(0));
-    const visited = Array.from(Array(N), () => Array(M).fill(false));
-
-    for(let j =0; j < K; j++){
-        const [x, y] = input.shift().split(" ").map(Number);
-        graph[y][x] = 1;
-    }
-
-    let count = 0;
-    for(let y = 0; y < N; y++){
-        for(let x = 0; x< M; x++){
-            if(graph[y][x] === 1 && !visited[y][x]){
-                dfs(x, y, graph, visited, N, M);
-                count++;
-            }
-        }
-    }
-
-    answer.push(count);
-
+for(let i = 0; i < N; i++){
+    graph[i] = input[i].split("").map(Number);
 }
 
+const bfs = (x, y) => {
+    const q = [];
+    q.push([x, y]);
+    visited[x][y] = true;
 
-function dfs(x, y, graph, visited, N, M) {
-    if(graph[y][x] === 1 && visited[y][x] === false){
-        visited[y][x] = true;
-
-        for(let i =0; i <4; i++){
-            const [newY, newX] = [y + dy[i], x + dx[i]];
-            if(newY >= 0 && newY < N && newX >= 0 && newX < M ){
-                dfs(newX, newY, graph, visited, N, M);
+    while(q.length){
+        const [x1, y1] = q.shift();
+        for(let i =0; i < 4; i++){
+            const [nx, ny] = [x1 + dx[i], y1 + dy[i]];
+            if(nx >= 0 && nx < N && ny >= 0 && ny < M){
+                if(graph[nx][ny] === 1 && !visited[nx][ny]){
+                    visited[nx][ny] = true;
+                    graph[nx][ny] = graph[x1][y1] + 1; //거리누적
+                    q.push([nx, ny]);
+                }
             }
         }
     }
 }
 
-answer.map(c => console.log(c));
+bfs(0,0);
+console.log(graph[N-1][M-1]);
---
+++ boj_2178.js
@@ -0,0 +1,34 @@
+
+const [N, M] = input.shift().split(" ").map(Number);
+const graph = new Array(N);// visited배열 생성
+const visited = Array.from(Array(N), () => Array(M).fill(false)); // visited배열 생성
+
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+
+for(let i = 0; i < N; i++){
+    graph[i] = input[i].split("").map(Number);
+}
+
+const bfs = (x, y) => {
+    const q = [];
+    q.push([x, y]);
+    visited[x][y] = true;
+
+    while(q.length){
+        const [x1, y1] = q.shift();
+        for(let i =0; i < 4; i++){
+            const [nx, ny] = [x1 + dx[i], y1 + dy[i]];
+            if(nx >= 0 && nx < N && ny >= 0 && ny < M){
+                if(graph[nx][ny] === 1 && !visited[nx][ny]){
+                    visited[nx][ny] = true;
+                    graph[nx][ny] = graph[x1][y1] + 1; //거리누적
+                    q.push([nx, ny]);
+                }
+            }
+        }
+    }
+}
+
+bfs(0,0);
+console.log(graph[N-1][M-1]);
