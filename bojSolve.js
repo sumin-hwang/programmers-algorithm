435a5b12dd7d50458c0c08e1b1d606b4b7b23f43
sumin-hwang <suumiin.h@gmail.com>
2025-11-25 23:11:44 +0900

    백준 10799 쇠막대기 (스택)

--- bojSolve.js
+++ bojSolve.js
@@ -26,27 +26,9 @@ let input = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\pr
 let input1 = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().split('\n').map(c => c.split(" ").map(c => Number(c)));
 
 let str = input[0];
-const bomb = input[1];
-
+let count = 0;
 const stack = [];
 
-for(let ch of str){
-  stack.push(ch);
-  let match = true;
+for(let i =0; i < input.length; 9)
 
-  if(stack.length >= bomb.length){
-    for(let i =0; i < bomb.length; i++){
-      if(stack[stack.length - bomb.length + i] !== bomb[i]){
-        match = false;
-        break;
-      }
-    }
-
-    if(match){
-      stack.length -= bomb.length;
-    }
-  }
-}
-
-const result = stack.join("");
-console.log(result === "" ? "FRULA" : result);
+console.log(count);
