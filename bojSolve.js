2a191ad5c2599bbd10fa74a6ab796e153df1d156
sumin-hwang <suumiin.h@gmail.com>
2025-12-09 23:17:03 +0900

    dfs 연습하기

--- bojSolve.js
+++ bojSolve.js
@@ -27,52 +27,13 @@ let inputSpaceNumber = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\al
 let input = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().trim().split('\n');
 let input1 = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().split('\n').map(c => c.split(" ").map(c => Number(c)));
 
-const N = Number(input.shift());
-const map = new Array(N);
-const visited = Array.from(Array(N), () => Array(N).fill(0));
+const [M, N, K] = input.shift().split(" ").map(Number);
+const farm = Array.from(Array(N), () => Array(M).fill(0));
+const visited = Array.from(Array(N), () => Array(M).fill(false));
 
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, -1, 1];
-
-for(let i =0; i < input.length; i++){
-  map[i] = input[i].split("").map(Number);
+for(let str of input){
+  const [x, y] = str.split(" ").map(Number);
+  farm[y][x] = 1;
 }
 
-
-const dfs = (x, y) => {
-  let count = 1;
-  const stack = [[x, y]];
-  visited[x][y] = true;
-
-  while(stack.length){
-    const [cx, cy] = stack.pop();
-
-    for(let i =0; i < 4; i++){
-      const [nx, ny] = [cx + dx[i], cy + dy[i]];
-
-      if(nx >= 0 && nx < N && ny >= 0 && ny < N){ 
-        if(!visited[nx][ny] && map[nx][ny] === 1){
-          stack.push([nx, ny]);
-        visited[nx][ny] = true;
-        count++;
-        }
-        
-      }
-    }
-  }
-  return count;
-}
-
-const result = [];
-
-for(let i =0; i < N; i++){
-  for(let j =0; j < N; j++){
-    if(map[i][j] === 1 && !visited[i][j]){
-      result.push(dfs(i, j));
-    }
-  }
-}
-
-result.sort((a,b) => a - b);
-console.log(result.length);
-console.log(result.join("\n"));
+console.log(farm);
