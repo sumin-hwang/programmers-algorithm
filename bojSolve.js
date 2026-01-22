b87b9c3ffc98c9389b64e7e4a45c223744e21cd0
sumin-hwang <suumiin.h@gmail.com>
2026-01-22 23:28:14 +0900

    백준 binarySearch 2

--- bojSolve.js
+++ bojSolve.js
@@ -27,33 +27,29 @@ let inputSpaceNumber = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\al
 let input = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().trim().split('\n');
 let input1 = require('fs').readFileSync('C:\\Users\\hsm95\\vscode_\\algorithm\\programmers-algorithm\\example.txt').toString().split('\n').map(c => c.split(" ").map(c => Number(c)));
 
-const N = Number(input.shift());
+const [N, M] = input.shift().split(" ").map(Number);
 let arr = input[0].split(" ").map(Number);
-const M = Number(input[1]);
-const quiz = input[2].split(" ").map(Number);
 
 arr = arr.sort((a,b) => a - b);
 
-let answer = [];
+let start = 0;
+let end = arr[arr.length - 1];
 
-const binarySearch = (arr, target) => {
-    let start = 0;
-    let end = arr.length - 1;
+while(start <= end){
+    const mid = Math.floor((start + end) / 2);
+    let trees = 0;
 
-    while(start <= end){
-        let mid = Math.floor((start +end)/ 2);
-
-        if(target < arr[mid]){
-            end = mid -1;
-        }else if (target > arr[mid]){
-            start = mid + 1;
-        }else{
-            return 1;
+    for(let i=0; i < arr.length; i++){
+        if(mid < arr[i]){
+            trees += arr[i] - mid;
         }
     }
 
-    return 0;
+    if(trees >= M){
+        start = mid + 1;
+    }else{
+        end = mid - 1;
+    }
 }
 
-quiz.forEach((e) => console.log(binarySearch(arr, e)));
-
+console.log(end);
