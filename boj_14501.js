b9e5fb40b689c23cf97b44d6756d0c890603a385
sumin-hwang <suumiin.h@gmail.com>
2026-02-25 23:40:04 +0900

    백준 14501 코드 수정

--- boj_14501.js
+++ boj_14501.js
@@ -1,25 +1,24 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
-
 const N = Number(input.shift());
+const dp = new Array(N + 1).fill(0);
 const T = [];
 const P = [];
 
-for(let i = 0; i < N; i++){
-  const [t, p] = input[i].split(' ').map(Number);
-  T.push(t);
-  P.push(p);
+for(let i =0; i < N; i++){
+    let [t, p] = input[i].split(" ").map(Number);
+    T.push(t);
+    P.push(p);
 }
 
+for(let i = N-1; i >=0; i--){
+    const nextDay = i + T[i]; //상담 끝나는 날
 
-const dp = Array(N + 1).fill(0);
-
-for(let i = N-1; i >= 0 ;i--){
-  if(i + T[i] > N){
-    dp[i] = dp[i + 1];
-  }else{
-    dp[i] = Math.max(dp[i + 1], P[i] + dp[i + T[i]]);
-  }
+    if(nextDay <= N){
+        dp[i] = Math.max(dp[i + 1], P[i] + dp[nextDay]);
+    }else{
+        dp[i] = dp[i + 1];
+    }
 }
 
 console.log(dp[0]);
