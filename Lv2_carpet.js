067819c42f16ebc1142f90456355a41d4e969202
sumin-hwang <suumiin.h@gmail.com>
2026-05-14 23:14:34 +0900

    부분 수열 합 구하기

---
+++ Lv2_carpet.js
@@ -0,0 +1,16 @@
+function solution(brown, yellow) {
+    var answer = [];
+   
+    const total = brown + yellow;
+    
+    for(let h = 3; h <= total; h++){
+        if(total%h === 0){
+            let w = total/h;
+            
+            if((w -2) * (h - 2) === yellow){
+                return [w, h];
+            }
+        }
+    }
+    
+}
